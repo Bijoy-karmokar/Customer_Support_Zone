233bc82ac9cdb19bdedd81b89711ca7e5cb232f0
Bijoy-karmokar <bijoykarmokar320@gmail.com>
2026-03-03 22:33:22 +0600

    Navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,88 @@
+import React from "react";
+import { FaPlus } from "react-icons/fa";
+
+const Navbar = () => {
+  return (
+    <div className="navbar bg-base-100 shadow-sm">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              {" "}
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />{" "}
+            </svg>
+          </div>
+          <ul
+            tabIndex="-1"
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 text-lg font-semibold w-52 p-2 shadow"
+          >
+            <li>
+              <a>Home</a>
+            </li>
+            <li>
+              <a>FAQ</a>
+            </li>
+            <li>
+              <a>Changelog</a>
+            </li>
+            <li>
+              <a>Blog</a>
+            </li>
+            <li>
+              <a>Download</a>
+            </li>
+            <li>
+              <a>Contact</a>
+            </li>
+          </ul>
+        </div>
+        <a className="text-2xl font-semibold">CS-Ticket System</a>
+      </div>
+      <div className="navbar-end  hidden lg:flex">
+        <ul className="menu menu-horizontal text-lg font-medium px-1">
+          <li>
+            <a>Home</a>
+          </li>
+          <li>
+            <a>FAQ</a>
+          </li>
+          <li>
+            <a>Changelog</a>
+          </li>
+          <li>
+            <a>Blog</a>
+          </li>
+          <li>
+            <a>Download</a>
+          </li>
+          <li>
+            <a>Contact</a>
+          </li>
+        </ul>
+
+        <button
+          className="px-5 py-2.5 relative rounded group font-medium text-white font-medium inline-block"
+        >
+          <span className="absolute top-0 left-0 w-full h-full rounded opacity-50 filter blur-sm bg-gradient-to-br from-purple-600 to-blue-500"></span>
+          <span className="h-full w-full inset-0 absolute mt-0.5 ml-0.5 bg-gradient-to-br filter group-active:opacity-0 rounded opacity-50 from-purple-600 to-blue-500"></span>
+          <span className="absolute inset-0 w-full h-full transition-all duration-200 ease-out rounded shadow-xl bg-gradient-to-br filter group-active:opacity-0 group-hover:blur-sm from-purple-600 to-blue-500"></span>
+          <span className="absolute inset-0 w-full h-full transition duration-200 ease-out rounded bg-gradient-to-br to-purple-600 from-blue-500"></span>
+          <span className="relative flex items-center gap-2"><FaPlus></FaPlus> New Ticket</span>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
